e943264def80687c5d3156998e0a433a766f66cf
pacino <haitaohhh28@gmail.com>
2024-10-03 21:47:06 +0800

    feat(nuxt.config): 增加配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  app: {
+    baseURL: "/",
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width,initial-scale=1.0,user-scalable=0",
+        },
+        {
+          charset: "utf-8",
+        },
+      ],
+      link: [],
+      style: [],
+      script: [],
+      noscript: [],
+    },
+  },
+  css: [
+    "~/assets/font/iconfont.css",
+    "~/assets/animate.css",
+    "~/assets/highlight.css",
+  ],
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      isClient: true,
+    },
+    app: {
+      BASE_URL: "https://www.haoht123.com",
+    },
+  },
+  nitro: {},
+  modules: ["@element-plus/nuxt"],
+  build: {
+    transpile: [
+      "@fortawesome/fontawesome-svg-core",
+      "@fortawesome/pro-solid-svg-icons",
+      "@fortawesome/pro-regular-svg-icons",
+      "@fortawesome/pro-light-svg-icons",
+      "@fortawesome/free-brands-svg-icons",
+    ],
+  },
+});
